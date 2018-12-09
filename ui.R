ddf4d7f3881ea56055889a9783be3de8ce66c3c2
aliu26 <amy_liu1@brown.edu>
2018-12-09 15:17:40 -0500

    update ui with slider

--- ui.R
+++ ui.R
@@ -18,7 +18,29 @@ navbarPage(
            p("*")),
   tabPanel("Table"),
   tabPanel("Graphs",  tabsetPanel(
-    tabPanel("Word Cloud"),
+    tabPanel("Word Cloud", 
+             fluidPage(
+      # Application title
+               titlePanel("Word Cloud"),
+               sidebarLayout(
+        # Sidebar with a slider and selection inputs
+        sidebarPanel(
+          selectInput("selection", "Choose a Coffee Shop:", choices = ""),
+          hr(),
+          sliderInput("freq",
+                      "Minimum Frequency:",
+                      min = 1,  max = 50, value = 15),
+          sliderInput("max",
+                      "Maximum Number of Words:",
+                      min = 1,  max = 300,  value = 100)
+        ),
+        
+        # Show Word Cloud
+        mainPanel(
+          plotOutput("plot")
+        )
+      )
+    )),
     tabPanel("xx"),
     tabPanel("xx")
     )
