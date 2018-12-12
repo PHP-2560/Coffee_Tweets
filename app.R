6c58ac70ce3689f1486bb2b3fe770e72519fd746
erinbugbee <erin_bugbee@brown.edu>
2018-12-12 14:24:10 -0500

    Add markdown

--- app.R
+++ app.R
@@ -1,7 +1,3 @@
-check_packages(c("shiny", "shinythemes", "ggplot2", "devtools", "twitteR", "base64enc", "DT"))
-install_github("PHP-2560/r-package-aes/coffee")
-library(coffee)
-
 ui <- dget("ui.R")
 server <- dget("server.R")
 shinyApp(ui = ui, server = server)
